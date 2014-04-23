50f98e12cdac465acd6cc841cc196e756ea69b7f
Drake Tetreault <drake@ivcomputer.com>
2014-04-23 15:14:14 -0700

    Started work on Driver.

--- Project2/Program.cs
+++ Project2/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 namespace Project2
 {
-	internal class Program
+	internal static class Program
 	{
 		private static void Main(string[] args)
 		{
@@ -13,6 +14,56 @@ namespace Project2
 			}
 			memory.Request(20, out allocation, out holesExamined);
 			Console.Read();
+		}
+
+		private static void Driver(
+			double averageRequestSize,
+			double standardDeviation,
+			Random random,
+			int simulationSteps,
+			int memorySize,
+			MemoryStrategy strategy)
+		{
+			// Setup driver.
+			var mm = new MemoryManager(memorySize, strategy);
+			var reserved = new List<int>();
+
+			// Statistics
+			int totalHolesExamined = 0;
+
+			for (int i = 0; i < simulationSteps; i++)
+			{
+				int requestSize = random.NextGaussian(averageRequestSize, standardDeviation, 1, memorySize);
+				int allocation, holesExamined;
+				while (mm.Request(requestSize, out allocation, out holesExamined))
+				{
+					reserved.Add(allocation);
+					int placeToSwap = random.Next(reserved.Count);
+					reserved[reserved.Count - 1] = reserved[placeToSwap];
+					reserved[placeToSwap] = allocation;
+
+					totalHolesExamined += holesExamined;
+
+					requestSize = random.NextGaussian(averageRequestSize, standardDeviation, 1, memorySize);
+				}
+				totalHolesExamined += holesExamined;
+			}
+		}
+
+		private static int NextGaussian(this Random random, double mean, double stDev, int min, int max)
+		{
+			int gaussian;
+			do
+			{
+				double u1 = random.NextDouble(),
+				u2 = random.NextDouble();
+				while (u1 == 0)
+					u1 = random.NextDouble();
+				while (u2 == 0)
+					u2 = random.NextDouble();
+				gaussian = (int)Math.Round(stDev * Math.Sqrt(-2.0 * Math.Log(u1, Math.E)) * Math.Cos(2.0 * Math.PI * u2) + mean);
+			} while (gaussian < min || gaussian > max);
+			return gaussian;
 		}
 	}
 }
