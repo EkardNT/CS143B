6d8fc3c184835a688d5d4d94894b06ac66e5677d
Drake Tetreault <ekardnt@ekardnt.com>
2014-04-24 07:57:34 -0700

    Implemented the remaining 3 memory strategies.

--- Project2/MemoryStrategy.cs
+++ Project2/MemoryStrategy.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Project2
+﻿namespace Project2
 {
 	public delegate bool MemoryStrategy(
 		int headSegment,
@@ -11,36 +9,136 @@ namespace Project2
 
 	public static class MemoryStrategies
 	{
-		public static MemoryStrategy FirstFit()
+		public static MemoryStrategy FirstFit
 		{
-			return (int headSegment, int[] mainMemory, int minSegmentSize, out int segmentAddress, out int segmentsInspected) =>
+			get
 			{
-				segmentsInspected = 0;
-				segmentAddress = headSegment;
-				while(segmentAddress != MemoryManager.NullPointer)
+				return (int headSegment, int[] mainMemory, int minSegmentSize, out int segmentAddress, out int segmentsInspected) =>
 				{
-					segmentsInspected++;
-					if (-mainMemory[segmentAddress] >= minSegmentSize)
-						return true;
-					segmentAddress = mainMemory[MemoryManager.NextPtrAddr(segmentAddress)];
-				}
-				return false;
-			};
+					segmentsInspected = 0;
+					segmentAddress = headSegment;
+					while (segmentAddress != MemoryManager.NullPointer)
+					{
+						segmentsInspected++;
+						if (-mainMemory[segmentAddress] >= minSegmentSize)
+							return true;
+						segmentAddress = mainMemory[MemoryManager.NextPtrAddr(segmentAddress)];
+					}
+					return false;
+				};
+			}
 		}
 
-		public static MemoryStrategy NextFit()
+		public static MemoryStrategy NextFit
 		{
-			throw new NotImplementedException();
+			get
+			{
+				int currentSegmentAddr = MemoryManager.NullPointer;
+				return (int headSegment, int[] mainMemory, int minSegmentSize, out int segmentAddress, out int segmentsInspected) =>
+				{
+					segmentsInspected = 0;
+					segmentAddress = MemoryManager.NullPointer;
+
+					if (currentSegmentAddr == MemoryManager.NullPointer)
+						currentSegmentAddr = headSegment;
+
+					bool startedAtHead = currentSegmentAddr == headSegment;
+					int startAddr = currentSegmentAddr;
+
+					while (currentSegmentAddr != MemoryManager.NullPointer)
+					{
+						segmentsInspected++;
+						if (-mainMemory[currentSegmentAddr] >= minSegmentSize)
+						{
+							segmentAddress = currentSegmentAddr;
+							currentSegmentAddr = mainMemory[MemoryManager.NextPtrAddr(currentSegmentAddr)];
+							return true;
+						}
+						currentSegmentAddr = mainMemory[MemoryManager.NextPtrAddr(currentSegmentAddr)];
+					}
+					// Loop back around.
+					if (!startedAtHead)
+					{
+						currentSegmentAddr = headSegment;
+						while (currentSegmentAddr != startAddr)
+						{
+							segmentsInspected++;
+							if (-mainMemory[currentSegmentAddr] >= minSegmentSize)
+							{
+								segmentAddress = currentSegmentAddr;
+								currentSegmentAddr = mainMemory[MemoryManager.NextPtrAddr(currentSegmentAddr)];
+								return true;
+							}
+							currentSegmentAddr = mainMemory[MemoryManager.NextPtrAddr(currentSegmentAddr)];
+						}
+					}
+					return false;
+				};
+			}
 		}
 
-		public static MemoryStrategy BestFit()
+		public static MemoryStrategy BestFit
 		{
-			throw new NotImplementedException();
+			get
+			{
+				return (int headSegment, int[] mainMemory, int minSegmentSize, out int segmentAddress, out int segmentsInspected) =>
+				{
+					segmentsInspected = 0;
+					segmentAddress = MemoryManager.NullPointer;
+					int minSize = int.MaxValue,
+						currentAddress = headSegment,
+						segmentSize;
+					while (currentAddress != MemoryManager.NullPointer)
+					{
+						segmentsInspected++;
+						segmentSize = -mainMemory[currentAddress];
+						if (segmentSize == minSegmentSize)
+						{
+							segmentAddress = currentAddress;
+							return true;
+						}
+						if (segmentSize >= minSegmentSize && segmentSize < minSize)
+						{
+							minSize = segmentSize;
+							segmentAddress = currentAddress;
+						}
+						currentAddress = mainMemory[MemoryManager.NextPtrAddr(currentAddress)];
+					}
+					return minSize != int.MaxValue;
+				};
+			}
 		}
 
-		public static MemoryStrategy WorstFit()
+		public static MemoryStrategy WorstFit
 		{
-			throw new NotImplementedException();
+			get
+			{
+				return (int headSegment, int[] mainMemory, int minSegmentSize, out int segmentAddress, out int segmentsInspected) =>
+				{
+					segmentsInspected = 0;
+					segmentAddress = MemoryManager.NullPointer;
+					int maxSize = int.MinValue,
+						currentAddress = headSegment,
+						segmentSize;
+					while (currentAddress != MemoryManager.NullPointer)
+					{
+						segmentsInspected++;
+						segmentSize = -mainMemory[currentAddress];
+						if (segmentSize == minSegmentSize)
+						{
+							segmentAddress = currentAddress;
+							return true;
+						}
+						if (segmentSize >= minSegmentSize && segmentSize > maxSize)
+						{
+							maxSize = segmentSize;
+							segmentAddress = currentAddress;
+						}
+						currentAddress = mainMemory[MemoryManager.NextPtrAddr(currentAddress)];
+					}
+					return maxSize != int.MinValue;
+				};
+			}
 		}
 	}
 }
