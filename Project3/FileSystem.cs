22551f9599372298145151f0bd8d79ba8f79b5e9
Drake Tetreault <drake@ivcomputer.com>
2014-05-19 08:48:38 -0700

    Created LogicalDisk class and stub of FileSystem.

---
+++ Project3/FileSystem.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections.Generic;
+namespace Project3
+{
+	public interface IFileSystem
+	{
+		/// <summary>
+		/// Creates a new file with the given logical file name.
+		/// The file name must not be longer than 4 bytes when
+		/// encoded in UTF-8.
+		/// </summary>
+		void Create(string fileName);
+
+		/// <summary>
+		/// Destroys the file with the given logical file name.
+		/// </summary>
+		void Destroy(string fileName);
+
+		/// <summary>
+		/// Opens a file for reading and writing, give the file's
+		/// logical name. Returns the file handle which identifies
+		/// the newly opened file.
+		/// </summary>
+		int Open(string fileName);
+
+		/// <summary>
+		/// Closes a file given its file handle.
+		/// </summary>
+		void Close(int file);
+
+		/// <summary>
+		/// Reads a sequence of bytes from a file into a destination buffer.
+		/// The file's data pointer is advanced by the number of bytes read.
+		/// Returns the number of bytes read.
+		/// </summary>
+		int Read(int file, byte[] destination, int count);
+
+		/// <summary>
+		/// Writes a sequence of bytes to a file from a source buffer. The
+		/// file's data pointer by the number of bytes written. Returns the
+		/// number of bytes written.
+		/// </summary>
+		int Write(int file, byte[] source, int count);
+
+		/// <summary>
+		/// Updates a file's data pointer to the given position value.
+		/// </summary>
+		void Seek(int file, int position);
+
+		/// <summary>
+		/// Returns the logical names of all currently existent files.
+		/// </summary>
+		IEnumerable<string> Directory();
+
+		/// <summary>
+		/// Resets the file system's state to its initial value. Optionally
+		/// restores state from a serialization file created by a previous call
+		/// to Save() if the given serializationFilePath is not null.
+		/// </summary>
+		void Init(string serializationFilePath = null);
+
+		/// <summary>
+		/// Creates a serialization file at the given path that contains current
+		/// state of the file system, suitable for restoring later by calling Init()
+		/// with the same file path.
+		/// </summary>
+		void Save(string serializationFilePath);
+	}
+
+	public class FileSystem : IFileSystem
+	{
+		public void Create(string fileName)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Destroy(string fileName)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public int Open(string fileName)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Close(int file)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public int Read(int file, byte[] destination, int count)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public int Write(int file, byte[] source, int count)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Seek(int file, int position)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public IEnumerable<string> Directory()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Init(string serializationFilePath = null)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Save(string serializationFilePath)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
---
+++ Project3/LogicalDisk.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace Project3
+{
+	public interface ILogicalDisk
+	{
+		/// <summary>
+		/// Gets the number of logical blocks in the logical disk.
+		/// </summary>
+		int BlockCount { get; }
+		/// <summary>
+		/// Gets the size in bytes of a logical block.
+		/// </summary>
+		int BlockSize { get; }
+		/// <summary>
+		/// Reads data from a logical block into the given destination
+		/// buffer. The destination buffer must be at least as long
+		/// as the BlockSize.
+		/// </summary>
+		void ReadBlock(int block, byte[] destination);
+		/// <summary>
+		/// Writes data to a logical block from the given source buffer.
+		/// The source buffer must be at least as long as the BlockSize.
+		/// </summary>
+		void WriteBlock(int block, byte[] source);
+	}
+
+	public class LogicalDisk : ILogicalDisk
+	{
+		private readonly byte[,] disk;
+
+		public LogicalDisk(int blockCount, int blockSize)
+		{
+			disk = new byte[blockCount, blockSize];
+			BlockCount = blockCount;
+			BlockSize = blockSize;
+		}
+
+		public int BlockCount { get; private set; }
+		public int BlockSize { get; private set; }
+
+		public void ReadBlock(int block, byte[] destination)
+		{
+			if (destination == null)
+				throw new ArgumentNullException("destination");
+			if (destination.Length < BlockSize)
+				throw new ArgumentException("Destination length is insufficient.", "destination");
+			for (int i = 0; i < BlockSize; i++)
+				destination[i] = disk[block, i];
+		}
+
+		public void WriteBlock(int block, byte[] source)
+		{
+			if (source == null)
+				throw new ArgumentNullException("source");
+			if (source.Length < BlockSize)
+				throw new ArgumentException("source length is insufficient.", "destination");
+			for (int i = 0; i < BlockSize; i++)
+				disk[block, i] = source[i];
+		}
+	}
+}
